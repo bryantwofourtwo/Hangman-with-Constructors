7347227dc8ac75e215a0f7bbea642b07dfaaad76
bryantwofourtwo <bryantwofourtwo@gmail.com>
2017-12-07 14:17:52 -0700

    added functionality, base working game

--- app.js
+++ app.js
@@ -1,1 +1,158 @@
 var inquirer = require("inquirer");
+var words = require("./game");
+var display = require("./letters");
+var check = require("./words");
+
+var currentWord;
+var remainingGuesses = 10;
+var wins = 0;
+var losses = 0;
+var showPlayer = [];
+var checkLetter = [];
+var wordsPlayed = [];
+
+// Select a random word 
+function selectRandomWord() {
+    var x = Math.floor(Math.random() * 26)
+    currentWord = words.possibleWords[x];
+    // only allows us to play words that haven't been plpayed already
+    if(wordsPlayed.includes(currentWord)) {
+        selectRandomWord();
+    } else {
+        showPlayer = new display(currentWord);
+        checkLetter = new check(currentWord);
+    }
+    wordsPlayed.push(currentWord);
+}
+// Restart the game
+function restartGame() {
+    inquirer.prompt([{
+        name: "play",
+        message: "Would you like to play again? (y or n)"
+    }]).then(function(answer) {
+        var confirm = answer.play.toLowerCase();
+        if(confirm === "y") {
+            remainingGuesses = 10;
+            checkLetter.letterGuessed = [];
+            currentWord = "";
+            checkLetter.currentWordArray = [];
+
+            selectRandomWord();            
+            console.log("***");
+            console.log("---------------------------------");
+            console.log("HANGMAN!");
+            console.log("---------------------------------");
+            console.log("Welcome friends, to Hangman, Internet Relater Terminology and Nomenclature");
+            console.log("Let\'s start the game...");            
+            console.log("Guess this word:");
+            showPlayer.originalDisplay();
+            console.log("");
+            console.log("Guesses Remaining: " + remainingGuesses);
+            console.log("");
+            guessLetters();
+        }
+        else if(confirm == "n") {
+            console.log("It was a pleasure to serve!");
+            console.log("***")
+        } else {
+        console.log("Please select \"y\"  or \"n\"");
+        console.log("***");
+        restartGame();
+        }
+    })    
+};
+
+// Start Game!
+selectRandomWord();
+
+// Original Display
+console.log("");
+console.log("***");
+console.log("---------------------------------");
+console.log("HANGMAN!");
+console.log("---------------------------------");
+console.log("Welcome friends, to Hangman, Internet Relater Terminology and Nomenclature");
+console.log("Let\'s start the game...");
+console.log("***");
+console.log("Guess this word:");
+showPlayer.originalDisplay();
+console.log("");
+console.log("Guesses Remaining: " + remainingGuesses);
+console.log("");
+// end original deisplay
+
+// Prompt user to guess letter
+var guessLetters = function() {
+    if(remainingGuesses > 0) {
+        inquirer.prompt([{
+            name: "currentGuess",
+            message: "Guess a letter"
+        }]).then(function(answer) {
+            var letter = answer.currentGuess.toLowerCase();
+            var letters = /^[a-z]+$/;
+            // if the letter is valid
+            if(letter.match(letters)) {
+                if(checkLetter.lettersGuessed.includes(letter)){
+                    console.log('You have already guessed that letter');
+                    showPlayer.updatedDisplay(letter);
+                    console.log("Letters guessed: " + checkLetter.lettersGuessed);
+                    console.log("Guesses remaining: " + remainingGuesses);
+                    console.log("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+                    console.log("");
+                    guessLetters();
+                } else {
+                    checkLetter.lettersGuessed.push(letter);
+                    if(checkLetter.currentWordArray.includes(letter)) {
+                        console.log("Correct!");
+                        showPlayer.updatedDisplay(letter);
+                        if(showPlayer.updated == currentWord) {
+                            wins++;
+                            console.log("You win!!");
+                            console.log("***");
+                            console.log("Number of wins: " + wins);
+                            console.log("Number of losses: " + losses);
+                            console.log("***")
+                            restartGame();
+                        } else {
+                            console.log("Letters guessed: " + checkLetter.lettersGuessed);
+                            console.log("Remaining guesses: " + remainingGuesses);
+                            console.log("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+                            console.log("***");
+                            guessLetters();
+                        }
+                    } else {
+                            console.log("Wrong answer");
+                            showPlayer.updatedDisplay(letter);
+                            console.log("Letters guessed: " + checkLetter.lettersGuessed);
+                            remainingGuesses--;
+                            console.log("Remaining ruesses: " + remainingGuesses);
+                            console.log("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+                            console.log("***");
+                            guessLetters();
+                        }
+                    }
+                }
+            // if letter isn't valid
+            else {
+                console.log("Please select an alphabetic character, genius");
+                console.log("***");
+                guessLetters();
+            }
+        });
+        }
+        else {
+            losses++;
+            console.log("You lose");
+            console.log("The term we were looking for was: " + currentWord);
+            console.log("***");
+            console.log("Number of wins: " + wins);
+            console.log("Number of losses: " + losses);
+            console.log("***");
+            restartGame();
+        }
+}
+
+console.log("Updated");
+
+guessLetters();
+
--- letters.js
+++ letters.js
@@ -1,3 +1,32 @@
 //take in a random word as an argument and 
-//displays it as a string of dashes
+//displays it as a line of dashes
 
+Display = function(word) {
+    this.wordArray = word.split(""),
+    this.arrayofDashes = [],
+    this.updated,
+    this.originalDisplay = function() {
+        for(i=0; i<this.wordArray.length; i++) {
+            if(this.wordArray[i] === " ") {
+                x = " ";
+                this.arrayofDashes.push(x);
+            }
+            else {
+                x = "-";
+                this.arrayofDashes.push(x);
+            }
+        }
+        console.log(this.arrayofDashes.join(""));
+    },
+    this.updatedDisplay = function(letter) {
+        for(i=0; i<this.wordArray.length; i++) {
+            if(letter == this.wordArray[i]) {
+                this.arrayofDashes.splice(i, 1, letter);
+            }
+        }
+        this.updated = this.arrayofDashes.join("");
+        console.log(this.updated);
+    }
+}
+
+module.exports = Display;
