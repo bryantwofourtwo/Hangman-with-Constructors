77f3a994df10dd29c90360939d5fd120dc809d02
bryantwofourtwo <bryantwofourtwo@gmail.com>
2017-12-07 11:39:49 -0700

    added package.json and some code

--- words.js
+++ words.js
@@ -1,1 +1,9 @@
 // Check function to check if word has been played already
+Check = function(word){
+    this.currentWord = word,
+    this.currentWordArray = word.split(""),
+    this.lettersGuessed = []
+}
+
+module.exports = Check;
+
